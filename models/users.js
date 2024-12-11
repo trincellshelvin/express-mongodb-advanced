b6bafff1e5e6c13f940d79ca4b160f660396a98a
Trincell Shelvin <trincellshelvin@gmail.com>
2024-12-10 22:31:18 -0600

    added put and delete routes with images folder

--- models/users.js
+++ models/users.js
@@ -1,25 +1,12 @@
 import mongoose from 'mongoose';
 
 const userSchema = new mongoose.Schema({
-    name: {
-        type: String,
-        required: true
-    },
-    email: {
-        type: String,
-        required: true,
-        unique: true
-    },
-    age: {
-        type: Number,
-        required: false
-    },
-    isActive: {
-        type: Boolean,
-        default: true
-    }
+    name: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    age: { type: Number, required: false },
+    isActive: { type: Boolean, default: true }
 });
 
-const User = mongoose.model('User', userSchema);
+const user = mongoose.model('user', userSchema);
 
-export default User;
+export default user;
